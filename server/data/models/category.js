69c8202412403113f2e08e5ea617261e5f77dd5a
Aleksandra Stojcheva <astojceva@yahoo.com>
2016-01-09 15:14:03 +0200

    categories model edited

--- server/data/models/category.js
+++ server/data/models/category.js
@@ -2,9 +2,15 @@ var mongoose = require('mongoose');
 
 module.exports.init = function () {
     var categorySchema = new mongoose.Schema({
-        name: {type: String, require: '{PATH} is required', unique: true}
+        name: {type: String, require: true, unique: true},
+        products: [
+            {
+                name: {type: String, require: true},
+                price: {type: Number, require: true},
+                imageUrl: {type: String, required: true, unique: true}
+            }
+        ]
     });
-
     var Category = mongoose.model('Category', categorySchema);
 };
 
